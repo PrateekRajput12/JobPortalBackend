4874dd1de73f0b209c362e62c0c89f0d06fc02e0
PrateekRajput12 <ficredinn@gmail.com>
2025-05-25 08:06:37 +0530

    frontend

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 
 const corsOption = {
-    origin: 'http://localhost:5173',
+    origin: 'https://jobportal-five-lilac.vercel.app',
     credentials: true
 }
 app.use(cors(corsOption))
